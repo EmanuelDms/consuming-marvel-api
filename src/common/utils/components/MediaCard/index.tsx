0ac797bd9d383385440f42b17e37981c9dd16142
EmanuelDms <emanuel.duarte.maia@gmail.com>
2022-07-24 19:01:36 -0300

    feat: Change MediaCard component to include bookmark heart

--- src/common/utils/components/MediaCard/index.tsx
+++ src/common/utils/components/MediaCard/index.tsx
@@ -4,6 +4,8 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { CardHeader, IconButton } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 interface IProps {
   title: string;
@@ -14,19 +16,22 @@ interface IProps {
 export default function MediaCard({ image, title, description }: IProps) {
   return (
     <Card sx={{ maxWidth: 345 }} elevation={8}>
+      <CardHeader
+        action={
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+        }
+        title={title}
+      />
       <CardMedia component="img" image={image} alt={title} />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      {description && (
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      )}
     </Card>
   );
 }
